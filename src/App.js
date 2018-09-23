3011db6ee5b0950e3d52e9e9b2ea14e0bc1d2122
Maria Seljak <lseljak92@gmail.com>
2018-09-23 12:46:46 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  componentDidMount(){
+    this.loadMap()
+  }
+  loadMap=() => {
+    renderScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDZPYfsH0OQBZm7e-hULiD43MSrLEHtRdU&callback=initMap")
+    window.initMap=this.initMap
+  }
+  initMap=() => {
+  
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    })
+  }
+
+  render() {
+    return (
+      
+      <main>
+        <div id="map"></div>  
+      </main>
+    
+    )
+  }
+}
+
+  function renderScript(url) {
+    var index=window.document.getElementsByTagName("script")[0]
+    var script=window.document.createElement("script")
+    script.src=url
+    script.async=true
+    script.defer=true
+    index.parentNode.insertBefore(script, index)
+  }
+
+  export default App;
