2345d8595db60b7a2ef5e244e601dda7bbf30cc3
Maria Seljak <lseljak92@gmail.com>
2018-09-27 08:39:41 -0800

    Create Map class

---
+++ src/Map.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Map.css';
+
+class Map extends Component {
+   render() {
+        return(
+            <div id="map"></div>
+       )
+
+   }
+     
+} 
+
+export default Map
+
