33934d5f22b7272833ca6dcce0fccb1f12d55959
Maria Seljak <lseljak92@gmail.com>
2018-09-27 07:31:21 -0800

    Get venues using Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
 
 import React, { Component } from 'react';
 import './App.css';
+//Import axios to load and get venues from Foursquare API. Source. https://www.npmjs.com/package/axios
+import axios from 'axios'
 
 class App extends Component {
-  
-  componentDidMount(){
-    this.loadMap()
+
+  // Declare the state of the venues (array)
+  state= {
+    venues: []
   }
+
+  componentDidMount(){
+    this.getVenues()
+  }
+
+  //Load URL from Google Maps API. Source: https://developers.google.com/maps/documentation/javascript/tutorial
   loadMap=() => {
     renderScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDZPYfsH0OQBZm7e-hULiD43MSrLEHtRdU&callback=initMap")
     window.initMap=this.initMap
   }
+
+  //Get coffee venues close to North Beach in San Francisco using Foursquare API. Show a limit of 15 venues.
+  //Source: https://developer.foursquare.com/docs/api.
+  getVenues=() => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters={
+      client_id: "5HUYXTVATBZBJIRCO23F1SK2MAXO0UOXED1255F4LO04NKLI",
+      client_secret: "SCY5ZF5DZ4XY4J4K2IXV13AH1S4BQNQ0O33FJZ3TRHX1DTIE",
+      query: "coffee",
+      near: "North Beach, San Francisco",
+      limit: 15,
+      v: "20180323"
+
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          //Set the state for the venues array. Get data from Foursquare API and add it to the array. Then, loadMap
+          venues: response.data.response.groups[0].items
+        }, this.loadMap())
+      })
+      //Error handling
+      .catch(error => {
+        console.log("Something went wrong, please refresh the page. Error " + error)
+      })
+  }
+
+  //Initialize map element
   initMap=() => {
-  
+    //Create map variable with Google Maps API. Source: https://developers.google.com/maps/documentation/javascript/tutorial
     var map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 37.806053, lng: -122.410331},
+      zoom: 14
     })
+
+    //Create InfoWindows for each marker using Google Maps API. Source: https://developers.google.com/maps/documentation/javascript/infowindows
+    var infowindow = new window.google.maps.InfoWindow()
+      //Loop over each venue, add marker and infoWindow
+      this.state.venues.forEach(showVenue => {
+        
+      var contentString = `${showVenue.venue.name}`
+      
+      //instantiate the markers for each venue using the Google Maps API. Source: https://developers.google.com/maps/documentation/javascript/markers
+      var marker = new window.google.maps.Marker({
+        //Set marker's position based on the venue's lat and lng data from Foursquare's response
+        position: {lat: showVenue.venue.location.lat, lng: showVenue.venue.location.lng},
+        map: map,
+        title: showVenue.venue.name
+      })
+      /*Add event listener for each marker. When clicked, infoWindow opens. InfoWindow should close when another
+      marker is clicked.*/
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString)
+        infowindow.open(map, marker);
+      });
+    })
+
+
   }
 
   render() {
@@ -30,6 +92,8 @@ class App extends Component {
   }
 }
 
+  /*Load the Maps Javascript API by creating HTML script element with JavaScript. Referred to Elharony's tutorial video 
+  for guidance. Source: https://www.youtube.com/watch?v=W5LhLZqj76s&index=2&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1*/
   function renderScript(url) {
     var index=window.document.getElementsByTagName("script")[0]
     var script=window.document.createElement("script")
